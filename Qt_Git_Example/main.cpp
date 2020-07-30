b401d1f0c600e33b549898137a8107abb410db74
mohammadsilani <mohammadsilani@gmail.com>
2020-07-30 12:36:16 +0430

    This is the second commit

--- Qt_Git_Example/main.cpp
+++ Qt_Git_Example/main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    qInfo() << "Salam";
+    qInfo() << "Bye";
 
     return a.exec();
 }
